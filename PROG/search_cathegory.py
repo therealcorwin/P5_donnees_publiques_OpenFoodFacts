d33f9065ae97d53275f4d4c9ae2b32492567feef
Lyss74 <LyssProGm@gmail.com>
2019-01-09 18:11:49 +0100

    Update Categories

--- PROG/search_cathegory.py
+++ PROG/search_cathegory.py
@@ -7,16 +7,27 @@ from pprint import pprint
 
 # CONSTANTS #
 
-CATEGORIES = {"Charcuterie": 'Charcuterie',
-              "Boissons": 'boissons',
-              "Céréales": 'Céréales',
-              "Plats_préparé": 'Plats_préparé',
-              "Plats_surgelé": 'Plats_préparé',
-              "Biscuit": 'Biscuit',
-              "Viennoiseries": 'Viennoiseries'}
+CATEGORIES = ["Plats préparé",
+              "Plats surgelé",
+
+              "AlimentS d'origine végétales",
+              "bio",
+              "Céréales",
+
+              "Biscuits",
+              "Viennoiseries",
+              
+              "Desserts",
+              "Produit laitiers",
+              "Fromages",
+
+              "Charcuteries",
+              "viandes",
+              
+              "Boissons"]
 
 
-class ApiCollecting:
+class ApiCollectingData:
     """ Call the Api Open Food Fact """
 
     def __init__(self):
@@ -33,25 +44,27 @@ class ApiCollecting:
                       "tagtype_0": "categories",                                            # Get the result by category
                       'tag_0': category,                                         # the tag represents the article search
                       "tag_contains_0": "contains",
-                      "page_size": 10,                                                     # Number of articles per page
+                      "page_size": 50,                                                    # Number of articles per page
                       "json": 1}                                                              # The API response in JSON
 
             response = req.get(api, params=config)                           # Uses the configuration for the connection
             results = response.json()                                                      # Return the response in JSON
-            products = results['products']                                                       # Finally result of API
+            products_section = results['products']                                               # Finally result of API
 
-            for product in products:
+            for product in products_section:
                 product['main_category'] = category
 
-            all_products.extend(products)
+            all_products.extend(products_section)
+
+        # pprint(all_products)
 
         return all_products
 
-    def validate_the_data(self, keys, products):
-        """ Validates the complete fields """
+    def validate_the_data(self, keys, products_section):
+        """ Validate the complete fields """
 
         for key in keys:
-            if key not in products or not products[key]:
+            if key not in products_section or not products_section[key]:
                 return False
         return True
 
@@ -61,25 +74,31 @@ class ApiCollecting:
         product_final = []
         keys = ['categories', 'product_name_fr', 'nutrition_grade_fr', 'url', 'stores']
 
+        print(len(all_products))
+
         for product in all_products:
             if self.validate_the_data(keys, product):
+
                 categories = product['categories']
-                categorie = product['main_category']
+                format_categories = product['main_category']
                 name = product['product_name_fr']
                 grade = product['nutrition_grade_fr']
                 website = product['url']
                 store = product['stores']
-                key = (categorie, name, grade, website, store)
+                key = (format_categories.upper(), name, grade, website, store)
 
                 formatting = key
                 product_final.append(formatting)
+
         pprint(product_final)
+        print(len(product_final))
 
         return product_final
 
 
 def main():
-    call = ApiCollecting()
+
+    call = ApiCollectingData()
     connect = call.bring_out()
     final = call.format_final_response(connect)
     valid = call.validate_the_data(final, connect)
