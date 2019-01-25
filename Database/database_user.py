16779585c33e495b91219ff71f24645bdb3b5c4f
Lyss74 <LyssProGm@gmail.com>
2019-01-25 20:48:06 +0100

    Bug d'insertion Sub_key 0.3  -> Error [line 115 -> 137] 'result[key] = self._cmysql.convert_to_mysql(value)[0] _mysql_connector.MySQLInterfaceError: Python type list cannot be converted'

--- Database/database_user.py
+++ Database/database_user.py
@@ -2,22 +2,14 @@
 # -*- coding: Utf-8 -*-
 
 
-import records as rec
-from Database.database import *
+from Database.database import DataBaseCreator
 
 
 class DataBaseUser:
 
     def __init__(self):
-        self.db = None
-
-    """TEMP"""
-    def connect_mysql(self):
-        """ Connecting in the database """
-        self.db = rec.Database(f"mysql+mysqlconnector://{cons.USER}:{cons.PASSWORD}@localhost/"
-                               f"{cons.DATABASE}?charset=utf8mb4")
-        return self.db
-    """TEMP"""
+        self.database = DataBaseCreator()  # Load the database class
+        self.db = self.database.connect_mysql()
 
     def get_databases(self):
         """ Control the database """
@@ -46,20 +38,15 @@ class DataBaseUser:
 
 
 def main():
-
-    """ Connecting in the database """
+    """ Init the class """
     user = DataBaseUser()
-    connecting = user.connect_mysql()                                                    # Load the MySQL connexion
-
 
     """ Control the database """
     get_bases = user.get_databases()                                                      # Get the database list
-    # get_tables = user.get_tables()                                                           # Get the table list
-    # get_products = user.get_all_products()                                                  # Get the insert list
+    get_tables = user.get_tables()                                                           # Get the table list
+    get_products = user.get_all_products()                                                  # Get the insert list
 
     # choose = user.choose_database()
-
-    # choose = connecting.choose_database()
 
 
 if __name__ == "__main__":
