3d94e35572e5cd2aff2066089e18f23b413a2275
Lyss74 <LyssProGm@gmail.com>
2019-03-06 09:31:07 +0100

    Remove folder

---
+++ PROG/Api/search_cathegory.py
@@ -0,0 +1,94 @@
+# -*- PipEnv -*-
+# -*- coding: Utf-8 -*-
+
+import constants as cons
+
+from pprint import pprint
+
+import csv
+import requests as req
+
+
+class ApiCollectingData:
+    """ Call the Api Open Food Fact """
+
+    def __init__(self):
+        """ The constructor is not useful here """
+        pass
+
+    def bring_out(self):
+        """ Use the configuration for the connecting interface """
+        all_products = []
+        api = "https://fr.openfoodfacts.org/cgi/search.pl"                 # Address OpenFooFact.org the API FR locating
+
+        for category in cons.CATEGORIES:
+            config = {"action": "process",                                         # This config for  for connecting API
+                      "tagtype_0": "categories",                                            # Get the result by category
+                      'tag_0': category,                                         # the tag represents the article search
+                      "tag_contains_0": "contains",
+                      "page_size": 5,                                                     # Number of articles per page
+                      "json": 1}                                                              # The API response in JSON
+
+            response = req.get(api, params=config)                           # Uses the configuration for the connection
+            results = response.json()                                                      # Return the response in JSON
+            products_section = results['products']                                               # Finally result of API
+
+            for product in products_section:
+                product['main_category'] = category
+            all_products.extend(products_section)
+        # pprint(all_products)
+        return all_products
+
+    def validate_the_data(self, keys, products_section):
+        """ Validate the complete fields """
+        for key in keys:
+            if key not in products_section or not products_section[key]:
+                return False
+        return True
+
+    def format_final_response(self, all_products):
+        """ Formatted the response just harvest the categories selected """
+        product_final = []
+        keys = ['id', 'categories', 'product_name_fr', 'nutrition_grade_fr', 'stores', 'url']
+
+        print(len(all_products))
+        for product in all_products:
+            if self.validate_the_data(keys, product):
+                barre_code = product['id']
+                categories = product['categories']
+                format_categories = product['main_category']
+                name = product['product_name_fr']
+                grade = product['nutrition_grade_fr']
+                store = product['stores']
+                website = product['url']
+                key = (barre_code, format_categories.upper(), name, grade, store, website)
+                formatting = key
+                product_final.append(formatting)
+        # pprint(product_final)
+        # print(type(product_final))
+        return product_final
+
+    def convert_type_final(self, convert_type):
+        convert = tuple(convert_type)
+        print(convert)
+        return convert
+
+    def save_data(self, product_final, filename):
+        with open(filename, 'w', encoding='utf-8') as file:
+            writer = csv.writer(file, delimiter=';')
+            for e in product_final:
+                writer.writerow(e)
+        return filename
+
+
+def main():
+
+    name = ApiCollectingData()
+    connect = name.bring_out()
+    final = name.format_final_response(connect)
+
+    # save_in_file = call.save_data(final , 'output_data_save.csv')
+
+
+if __name__ == "__main__":
+    main()
---
+++ PROG/Database/database.py
@@ -0,0 +1,113 @@
+# -*- PipEnv -*-
+# -*- coding: Utf-8 -*-
+
+
+import records as rec
+
+import constants as cons
+import search_cathegory as search
+
+
+class DataBaseCreator:
+
+    def __init__(self):
+        pass
+
+    def connect_mysql(self):
+        """ Connecting in the database """
+        connect = rec.Database("mysql+mysqlconnector://%s:%s@localhost/%s?charset=utf8mb4"
+                               % (cons.USER, cons.PASSWORD, cons.DATABASE))
+        return connect
+
+    def show_database(self, connect):
+        """ Control the datanase """
+        databases = connect.query("SHOW DATABASES")
+        for row in databases:
+            print(row['Database'])
+        return databases
+
+    def show_table(self, connect):
+        """"""
+        tables = connect.query("SHOW TABLES")
+        for table in tables:
+            print(table)
+        return tables
+
+    def use_database(self, connect):
+        """"""
+        pass
+
+    def create_table_product(self, connect):
+        """ Create table """
+        connect.query("""CREATE TABLE Products_10k_Table (
+                                Barre_code TINYINT(13) PRIMARY KEY,
+                                Name_product VARCHAR(30),
+                                Grade CHAR(1),
+                                Web_site VARCHAR(255))""")
+        return connect
+
+    def create_table_categories(self, connect):
+        """"""
+        connect.query("""CREATE TABLE Categories (
+                                Categories VARCHAR(15))""")
+        return connect
+
+    def create_table_stores(self, connect):
+        """"""
+        connect.query("""CREATE TABLE Stores (
+                                Stores VARCHAR(50))""")
+        return connect
+
+    def create_favorites_table(self):
+        pass
+
+    def create_table(self, connecting):
+        """ Execute the creating table """
+        product = self.create_table_product(connecting)
+        categories = self.create_table_categories(connecting)
+        stores = self.create_table_stores(connecting)
+        return product, categories, stores
+
+    def insert_product(self, connect):
+        response_api = search.convert_type_final()
+        insert_product = """INSERT INTO Products_10k_Table (
+                            Barre_code,
+                            Name_product,
+                            Grade,
+                            name,
+                            Web_site) 
+                            VALUES (%s, %s, %s, %s, %s)"""
+        connect.query(insert_product, response_api)
+        return insert_product
+
+    def insert_categories(self):
+        insert_categories = """INSERT INTO Categories (
+                               Categories)
+                               VALUES (%s)"""
+        pass
+
+    def insert_stores(self):
+        insert_Stores = """INSERT INTO Stores (
+                          Stores) 
+                          VALUES (%s)"""
+        pass
+
+
+def main():
+    """ Connecting in the database """
+    databases = DataBaseCreator()
+    connecting = databases.connect_mysql()
+
+    """ Control the database """
+    # show = databases.show_table(connecting)
+    # choose = databases.choose_database(connecting)
+
+    """ Create table """
+    # categories = databases.create_table(connecting)
+
+    """ Insert data """
+    insert_p = databases.insert_product(connecting)
+
+
+if __name__ == "__main__":
+    main()
